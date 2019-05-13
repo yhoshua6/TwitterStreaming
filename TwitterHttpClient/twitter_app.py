c5c48283776d19a88d452ff4109c7b8227f4dfd2
JosueHer <yhoshua6@gmail.com>
2019-05-13 00:41:44 +0000

    afinn

--- TwitterHttpClient/twitter_app.py
+++ TwitterHttpClient/twitter_app.py
@@ -3,6 +3,9 @@ import sys
 import requests
 import requests_oauthlib
 import json
+import pykafka
+import pickle
+from afinn import Afinn
 
 # Replace the values below with yours
 ACCESS_TOKEN = '4431903384-sV7w5hZtiBxmqskOFwqYcxUflT1RXk9fEktc2ZV'
@@ -16,10 +19,19 @@ def send_tweets_to_spark(http_resp, tcp_connection):
     for line in http_resp.iter_lines():
         try:
             full_tweet = json.loads(line)
+
+            send_data = '{}'
+            my_data = json.loads(send_data)
+
+            my_data['text'] = full_tweet['text'].rstrip("\n")
+            my_data['senti_val'] = afinn.score(full_tweet['text'])
+
+            tweet = str(my_data['text']) + '\n' + str(my_data['senti_val']) + '\n'
+
             tweet_text = full_tweet['text']
             print("Tweet Text: " + tweet_text)
             print ("------------------------------------------")
-            tcp_connection.send(tweet_text + '\n')
+            tcp_connection.send(tweet)
         except:
             e = sys.exc_info()[0]
             print("Error: %s" % e)
@@ -27,16 +39,17 @@ def send_tweets_to_spark(http_resp, tcp_connection):
 
 def get_tweets():
     url = 'https://stream.twitter.com/1.1/statuses/filter.json'
-    #query_data = [('language', 'en'), ('locations', '-130,-20,100,50'),('track','#')]
-    query_data = [('locations', '-130,-20,100,50'), ('track', '#')]
+    query_data = [('language', 'en'), ('locations', '-130,-20,100,50'),('track','#')]
+    #query_data = [('locations', '-130,-20,100,50'), ('track', '#')]
     query_url = url + '?' + '&'.join([str(t[0]) + '=' + str(t[1]) for t in query_data])
     response = requests.get(query_url, auth=my_auth, stream=True)
     print(query_url, response)
     return response
 
 
+afinn = Afinn()
 TCP_IP = "edge.example.com"
-TCP_PORT = 9009
+TCP_PORT = 9008
 conn = None
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
